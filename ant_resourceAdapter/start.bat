7d885859c061073bb18e4eee5470c775d1ac775e
Edwin Biemond <biemond@gmail.com>
2012-02-26 15:44:09 +0100

    fixed resource adapter properties

--- ant_resourceAdapter/start.bat
+++ ant_resourceAdapter/start.bat
@@ -1,8 +1,5 @@
-#set ORACLE_HOME=C:\oracle\JDev11114PS
-#set JAVA_HOME=C:\oracle\jrockit-jdk1.6.0_26-R28
-#set ANT_HOME=%ORACLE_HOME%\jdeveloper\ant
-set ORACLE_HOME=C:\oracle\MiddlewarePS3
-set JAVA_HOME=C:\oracle\jrockit-jdk1.6.0_26-R28
+set ORACLE_HOME=C:\oracle\MiddlewarePS5
+set JAVA_HOME=C:\oracle\jrockit-jdk1.6.0_29-R28.2.2-4.1.0
 set ANT_HOME=%ORACLE_HOME%\modules\org.apache.ant_1.7.1
 set PATH=%ANT_HOME%\bin;%PATH%
 
